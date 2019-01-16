5785abcc5fcade1f1c7a26960a3afee29496d755
Rolf Locher <rlocher2@mac.com>
2019-01-15 22:42:55 -0500

    Initial Commit

---
+++ ScheduleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScheduleApp
+//
+//  Created by Rolf Locher on 1/15/19.
+//  Copyright © 2019 Rolf Locher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
